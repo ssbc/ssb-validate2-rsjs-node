82de514687306062ca69109cc03de512a832ce04
Andre Staltz <andre@staltz.com>
2021-06-04 23:03:46 +0300

    remove unnecessary cd in install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@
 # in order to build native modules, so we build our module here and move it
 # correctly
 if [ "$npm_config_platform" == "android" ]; then
-  cd native
   mkdir -p dist
   cargo build --release
-  cd ..
   cp ./target/$CARGO_BUILD_TARGET/release/libssb_validate2_rsjs_node.so ./dist/index.node
 elif [ "$npm_config_platform" == "ios" ]; then
-  cd native
   mkdir -p dist
   cargo build --release
-  cd ..
   cp ./target/$CARGO_BUILD_TARGET/release/libssb_validate2_rsjs_node.dylib ./dist/index.node
 fi
