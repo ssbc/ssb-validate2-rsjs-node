9b856fdede6cf169c8f278276f02df424a00fe80
Andre Staltz <andre@staltz.com>
2021-06-04 19:37:23 +0300

    fix filenames inside install.sh and postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,7 @@
 # On iOS nodejs-mobile we need index.node to be a folder that will be converted
 # to a .framework
 if [ "$npm_config_platform" == "ios" ]; then
-  mv native/dist/index.node native/dist/index
-  mkdir native/dist/index.node
-  mv native/dist/index native/dist/index.node/index
+  mv dist/index.node dist/index
+  mkdir dist/index.node
+  mv dist/index dist/index.node/index
 fi
